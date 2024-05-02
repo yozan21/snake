e21971dbdb1eab4d41af0cd1bcd11db0d4d68d3a
yozan21 <yojankaphle80@gmail.com>
2024-05-02 16:31:09 +0545

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Snake Using Perlin Noise.
+
+Welcome to this fun site. This site displays the use of perlin noise function in a visual form. Perlin noise function is a randomization function with a specific pattern. Each value is related to previous value i.e. present value is slightly larger or smaller than previous value this gives the illusion of controlled pattern.
+
+-Site-Link: [Live-Site](https://www.example.com)
---
+++ src/js/canvas.js
@@ -0,0 +1,76 @@
+import utils, { randomIntFromRange } from "./utils";
+import { noise, noiseSeed } from "@chriscourses/perlin-noise";
+import * as dat from "dat.gui";
+
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+// const gui = new dat.GUI();
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+// console.log(noise);
+const mouse = {
+  x: innerWidth / 2,
+  y: innerHeight / 2,
+};
+
+const colors = ["#2185C5", "#7ECEFD", "#FFF6E5", "#FF7F66"];
+
+const offset = {
+  x: 20,
+};
+// Objects
+class Circle {
+  constructor(x, y, radius, color, offSet) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.offSet = offSet;
+  }
+
+  draw() {
+    c.save();
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+    c.closePath();
+    c.restore();
+  }
+
+  update() {
+    this.draw();
+  }
+}
+
+let circles = [];
+for (let i = 0; i < 500; i++) {
+  circles.push(
+    new Circle(
+      -200,
+      -200,
+      10,
+      `hsl(${255 * (i / 500)}, 50%, 50%)`,
+      Math.random() * 2
+    )
+  );
+}
+let time1 = 0;
+let time2 = 0;
+// Animation Loop
+function animate() {
+  requestAnimationFrame(animate);
+  c.fillStyle = "rgba(0,0,0,0.02)";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  circles.forEach((circle) => {
+    circle.update();
+    circle.x = noise(time1 + circle.offSet + offset.x) * canvas.width;
+    circle.y = noise(time2 + circle.offSet) * canvas.height;
+  });
+
+  time1 += 0.003;
+  time2 += 0.005;
+}
+
+animate();
